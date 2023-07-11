fe5be694c63eab2fa1f29b1431cb02b62f913c19
Hüseyin Karadana <huseyinkaradana35@gmail.com>
2023-07-11 12:54:56 +0300

    Add project files.

---
+++ ShoppingListApp/Controllers/SessionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingListApp.ViewModels;
+using System.Data.SqlClient;
+namespace ShoppingListApp.Mapping
+{
+    public class SessionController : Controller
+    {
+        ViewModelMapping _mapper;
+    
+      
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(UserViewModel user)
+        {
+           //   UserViewModel users=_mapper.Map<UserViewModel>(user); 
+            return View();
+        }
+
+        
+    }
+}
---
+++ ShoppingListApp/Mapping/ViewModelMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ShoppingListApp.Models;
+using ShoppingListApp.ViewModels;
+
+namespace ShoppingListApp.Mapping
+{
+    public class ViewModelMapping : Profile
+    {
+        public ViewModelMapping()
+        {
+            CreateMap<User, UserViewModel>().ReverseMap();
+        }
+    }
+}
